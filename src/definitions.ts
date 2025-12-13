7dfa892d7209a71438221d049b1007e2e7838d34
Martin Donadieu <martindonadieu@gmail.com>
2025-12-13 07:44:18 +0000

    style: fix indentation for getPluginVersion method in TextInteractionPlugin interface

--- src/definitions.ts
+++ src/definitions.ts
@@ -9,13 +9,13 @@ export interface TextInteractionPlugin {
    */
   toggle(options: TextInteractionOptions): Promise<TextInteractionResult>;
 
-    /**
-     * Get the native Capacitor plugin version
-     *
-     * @returns {Promise<{ id: string }>} an Promise with version for this device
-     * @throws An error if the something went wrong
-     */
-    getPluginVersion(): Promise<{ version: string }>;
+  /**
+   * Get the native Capacitor plugin version
+   *
+   * @returns {Promise<{ id: string }>} an Promise with version for this device
+   * @throws An error if the something went wrong
+   */
+  getPluginVersion(): Promise<{ version: string }>;
 }
 
 export interface TextInteractionOptions {
